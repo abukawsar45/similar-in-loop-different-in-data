dcba03c08ef76d7382032a7e25d72a5aec0c77a7
abukawsar45 <kawsar1146236@gmail.com>
2023-03-21 11:49:00 +0600

    similar in loop different in data

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Aks></Aks>
+      <Aks></Aks>
+      <Abukawsar name='kawsar000' marrigeTarget='Keya'></Abukawsar>
+      <Abukawsar name="AbuKawSar222"></Abukawsar>
     </div>
   );
+}
+function Aks() {
+  return (
+    <div>
+      <h4>Abu Kaw Sar</h4>
+        <h1>okeykkk</h1>
+    </div>
+  )
+}
+function Abukawsar(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h3>Khuje: {props.marrigeTarget}</h3>
+    </div>
+  )
 }
 
 export default App;
